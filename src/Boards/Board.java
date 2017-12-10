3ebe349d01c1ac3c3715a5773bff65351b9e0e7c
BenW <benwagrez@gmail.com>
2017-12-09 18:07:44 -0600

    Added all Pieces classes and Board package

---
+++ src/Boards/Board.java
@@ -0,0 +1,18 @@
+package Boards;
+
+public class Board {
+	
+	int[][] boardValues = new int[8][8]; 
+			
+	public Board() {
+		
+		for(int yfiller = 0; filler<8; filler++) {
+			for(int xfiller = 0; filler<8; filler++) {
+				boardValues[y][x] = 0;
+			}
+		}
+		
+		
+	}
+	
+}
---
+++ src/Pieces/Rook.java
@@ -0,0 +1,15 @@
+package Pieces;
+
+public class Rook extends Piece{
+
+	 public boolean pathValid(int fX, int fY) {
+		 
+		 return true;
+	 }
+	  
+	  public int[][] pathDraw(int iX, int iY,int fX, int fY){
+		  int[][] Path = new int[1][1];//Instantiated with purpose of resolving errors - TBD
+		  return Path;
+	  }
+	
+}
