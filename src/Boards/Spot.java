67d941333799f258ed19129d13cee4b1c7df9426
narek <narek@192.168.86.39>
2017-12-09 19:22:56 -0600

    spotme

--- src/Boards/Spot.java
+++ src/Boards/Spot.java
@@ -1,5 +1,36 @@
 package Boards;
 
+import Pieces.*;
 public class Spot {
+    int x;
+    int y;
+    Piece piece;
+
+    public Spot(int x, int y) {
+        super();
+        this.x = x;
+        this.y = y;
+        piece = null;
+    }
+
+    public void occupySpot(Piece piece){
+        //if piece already here, delete it, i. e. set it dead
+        if(this.piece != null)
+            this.piece.setAvailable(false);
+        //place piece here
+        this.piece = piece;
+    }
+
+    public boolean isOccupied() {
+        if(piece != null)
+            return true;
+        return false;
+    }
+
+    public Piece releaseSpot() {
+        Piece releasedPiece = this.piece;
+        this.piece = null;
+        return releasedPiece;
+    }
 
 }
