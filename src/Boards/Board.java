b3198f950b0f9f14c1c9baf1fa77d90c5e7a9206
narek <narek@192.168.86.39>
2017-12-09 18:23:27 -0600

    ben hi its me ur mum

--- src/Boards/Board.java
+++ src/Boards/Board.java
@@ -8,10 +8,10 @@ public class Board {
 		
 		for(int yfiller = 0; yfiller<8; yfiller++) {
 			for(int xfiller = 0; xfiller<8; xfiller++) {
-				boardValues[y][x] = 0;
+				boardValues[yfiller][xfiller] = 0;
 			}
 		}
-		//ben is hawt
+		//Ben is hawt
 		
 	}
 	
