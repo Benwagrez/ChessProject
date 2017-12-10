627adc4b6aff535482e1c62e3a84702fe73d795b
BenW <benwagrez@gmail.com>
2017-12-09 18:57:35 -0600

    Introducing Spot class

--- src/Boards/Board.java
+++ src/Boards/Board.java
@@ -2,24 +2,36 @@ package Boards;
 
 public class Board {
 	
-	int[][] boardValues = new int[8][8]; 
+	Spot[][] boardValues = new Spot[8][8]; 
 			
 	public Board() {
 		
 		for(int yfiller = 0; yfiller<8; yfiller++) {
+			
 			for(int xfiller = 0; xfiller<8; xfiller++) {
 				boardValues[yfiller][xfiller] = 0;
 			}
+			
 		}
 		
 		/*
-		 * Fills the board with 0, designating an empty board
+		 *Fills the board with 0, designating an empty board
 		 */
 		
 	}
 	
 	/*
-	 * A constructor that initialized a new board
+	 *A constructor that initialized a new board
+	 */
+	
+	public void boardSetUp() {
+		
+		boardValues[][] = {2,3,4,5,6,4,3,2};
+		
+	}
+	
+	/*
+	 *Initializes the board array With a normal board set up
 	 */
 	
 }
---
+++ src/Boards/Spot.java
@@ -0,0 +1,5 @@
+package Boards;
+
+public class Spot {
+
+}
