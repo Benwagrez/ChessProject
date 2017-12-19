420b96093f5e6aa4b3a78b19fc6be83e55b34cc3
Benwagrez <benwagrez@gmail.com>
2017-12-18 18:00:03 -0600

    fuck me

--- src/Boards/ChessGame.java
+++ src/Boards/ChessGame.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 public class ChessGame extends JFrame implements MouseListener, MouseMotionListener {
   /**
-	 * fu
+	 * fuck me
 	 */
 	private static final long serialVersionUID = 1L;
 JLayeredPane layeredPane;
