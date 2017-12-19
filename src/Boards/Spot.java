85f51b0554f839a7018d3185e928b4b2017fcef2
SuperNar3k <ohanyanarek@gmail.com>
2017-12-19 12:33:12 -0600

    enpassant p2?

--- src/Boards/Spot.java
+++ src/Boards/Spot.java
@@ -2,8 +2,8 @@ package Boards;
 
 import Pieces.*;
 public class Spot {
-    int x, y;
-    Piece piece;
+    public int x, y;
+    public Piece piece;
     /*
      *Instance fields with x and y coordinates and the piece.
      */
@@ -18,11 +18,17 @@ public class Spot {
             this.piece = piece;
         }
     }
-    
+    //If there's no other piece on the new square, or if the colors of the two pieces are not the same, take the piece.
     public boolean isOccupied() {
         if(piece != null) {
         	return true;
         }
         return false;
+    }
+    //Check to see if new spot is occupied or not.
+    public void enPassant(Piece piece){
+    	if(this.piece == null || !(this.piece.color.equals(piece.color)) && (this.piece.enpassantable) && (spotValues[y+1][x].piece.name=="Pawn")) {
+            this.piece = piece;
+        }
     }
 }
