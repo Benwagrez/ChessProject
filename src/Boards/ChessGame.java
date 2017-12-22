78ce1584ffea0329f69deb91443df75033ad7803
SuperNar3k <ohanyanarek@gmail.com>
2017-12-21 19:38:53 -0600

    en passant 4.0

--- src/Boards/ChessGame.java
+++ src/Boards/ChessGame.java
@@ -198,9 +198,10 @@ public class ChessGame extends JFrame implements MouseListener, MouseMotionListe
 		            	} else if(turn=="Black") {
 		            		turn="White";
 		            	}
-		            	if(newGame.spotValues[iY][iX].piece.doingEnPassant){
+		            	if(/*newGame.spotValues[iY][iX].piece.doingEnPassant &&*/ newGame.spotValues[iY][iX].piece.name=="Pawn"){
 		            		takenchessPiece=(JLabel)((JPanel)c).getComponent(0);
 		            		JPanelGridLayout[iY][fX].remove(takenchessPiece);
+			            	newGame.spotValues[iY][fX].piece=null;
 		            	}
 		   		  } else {
 		   			  JPanelGridLayout[iY][iX].add(chessPiece);
